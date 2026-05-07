83a9adb1d77c7debdb26c376568513cd5aef8632
ronaldvester <59488390+ronaldvester@users.noreply.github.com>
2026-05-07 18:01:36 +0200

    Update stream_server.cpp

--- components/stream_server/stream_server.cpp
+++ components/stream_server/stream_server.cpp
@@ -287,7 +287,7 @@ void StreamServerComponent::dump_config() {
     std::string ip_str = "";
     for (auto &ip : network::get_ip_addresses()) {
       if (ip.is_set()) {
-        char buf[IP_ADDRESS_BUFFER_SIZE];
+        char buf[40];
         ip.str_to(buf);
         ip_str += " " + buf;
       }
